6560003c5c45ac93db6fe44285c169034c983632
srivasavi24 <srivasaviperavarapu@gmail.com>
2021-11-09 17:36:40 +0530

    vasavi

---
+++ dbwrite.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+client=MongoClient('127.0.0.1',27017)
+db=client['kits']
+c=db['ml']
+
+k=input('Enter msg:')
+dummy={'msg':k}
+c.insert_one(dummy)
+
+for i in c.find():
+     continue
+print(i)
---
+++ main.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+client=MongoClient('127.0.0.1',27017)
+db=client['kits']
+c=db['ml']
+
+
+for i in c.find():
+   print(i)
+
